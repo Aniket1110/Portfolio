ce0e31753f84acc2cc85f11ea6b5c37475266e8e
Aniket Majhi <aniketmajhi2001@gmail.com>
2021-06-10 21:36:12 +0530

    Modified

--- portfolio.js
+++ portfolio.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
   //skill sets
 
     let __count = 0;
-    arr1 = ["Programmer", "Developer","IIESTIAN"];
+    arr1 = ["Programmer", "Developer","IIESTian"];
     setInterval(function () {
       $("#skill_set").fadeOut(1000, function () {
         $(this).text(arr1[__count %  arr1.length]).fadeIn(1000);
@@ -28,17 +28,6 @@ $(document).ready(function () {
       });
     }, 2500);
 
-
-    //scooling
-
-   let c= 0;
-    arr2 = ["Sahebnagar High School (2011-2017)", "Palashipara MGS Vidyapith (2017-2019)", "IIEST SHIBPUR (2019-present)"];
-    setInterval(function () {
-      $("#content").fadeOut(1200, function () {
-        $(this).text(arr2[c % arr2.length]).fadeIn(1200);
-        c++;
-      });
-    }, 3000);
 
     // smooth scroll page 
 
